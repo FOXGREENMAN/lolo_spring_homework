944067fce8855c0e36bede0d036d15bf032ebab4
Lzh <420645952@qq.com>
2020-09-09 23:11:08 +0800

    add test

---
+++ src/main/java/com/lolo/dao/entity/UserExtVo.java
@@ -0,0 +1,41 @@
+package com.lolo.dao.entity;
+
+public class UserExtVo {
+
+    private  Integer uid;
+    private Integer invitedUid;
+    private String inviteCode;
+    private  String invitedCode;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public Integer getInvitedUid() {
+        return invitedUid;
+    }
+
+    public void setInvitedUid(Integer invitedUid) {
+        this.invitedUid = invitedUid;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+
+    public String getInvitedCode() {
+        return invitedCode;
+    }
+
+    public void setInvitedCode(String invitedCode) {
+        this.invitedCode = invitedCode;
+    }
+}
